9b8a3b2ffb7bd7db2ca4c2701c220ba9a3a08ac9
Shailesh-Tagadghar <shaileshtagadghar13@gmail.com>
2023-10-21 17:57:21 +0530

    File Input and Output example

---
+++ src/File_Input_Output/Demo.java
@@ -0,0 +1,9 @@
+package File_Input_Output;
+
+import java.io.FileInputStream;
+
+public class Demo {
+	public static void main(String[] args) {
+		FileInputStream fis = new FileInputStream("demo.txt");
+	}
+}
